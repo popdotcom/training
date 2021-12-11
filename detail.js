a5ac2b77b4e1e8508a0070ca7085b96e8ead2b9c
tkutky <monkeymouse98>
2021-12-11 21:04:46 +0700

    search and fix detail

--- detail.js
+++ detail.js
@@ -1,21 +1,18 @@
 var json = require('./listpet.json')
 var fs = require('fs')
 module.exports = function (req, res) {
-  var id = req.params.id
+  var id = parseInt(req.params.id)
   var check = false
   fs.readFile('listpet.json',function (err, data){
     var jsonData = JSON.parse(data)
     var list = jsonData.listPet
-    for(var i=0;i<list.length;i++){
-      if(list[i].id == id){
-        check = true
-        res.send(`
-        name: ${list[i].name} <br/>
-        available: ${list[i].available} <br/>
-        detail: ${list[i].detail} <br/>
-        `)
-      }
-    }
+    const detail = list.find(pet=> pet.id === id)
+    check = true;
+    res.send(`
+    name: ${detail.name} <br/>
+    available: ${detail.available} <br/>
+    <br/>
+    `)
     if(check == false){
       res.send("lỗi")
     }
--- home.js
+++ home.js
@@ -3,23 +3,34 @@ var fs = require('fs')
 
 module.exports = function (req, res) {
     var info ='';
+    var search = req.query.search
     fs.readFile('listpet.json',function (err, data){
         var jsonData = JSON.parse(data)
         var listPet = jsonData.listPet
         var info =''
-    for(var i = 0;i<listPet.length;i++)
-    {
-        if(listPet[i].available == true){
-            var avail = 'still have'
-        } else {
-            var avail = 'out of pet'
+    if(search !== undefined&&search !==''){
+        const listPets = listPet.find(pet => pet.name === search)
+        if (listPets !== undefined)
+        {
+            res.send(`
+            name: ${listPets.name} <br/>
+            available: ${listPets.available} <br/>
+            <br/>
+            `) 
+        } else 
+        {
+            res.send("cannot find ")
         }
+    } else {
+        for(var i = 0;i<listPet.length;i++)
+    {
         info +=`
         name: ${listPet[i].name} <br/>
-        available: ${avail} <br/>
+        available: ${listPet[i].available} <br/>
         <br/>
         `
     }
     res.send(info)
+    }
     })
 }
